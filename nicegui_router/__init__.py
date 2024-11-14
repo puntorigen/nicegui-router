18ab434f0a86dd749bfe358d253d900838f9ba84
Pablo Schaffner <pablo@puntorigen.com>
2024-11-14 09:04:56 -0300

    - added export for create_access_token to be able to use _auth suffix decorators

--- nicegui_router/__init__.py
+++ nicegui_router/__init__.py
@@ -1,4 +1,4 @@
 from .dynamic_router import NiceGUIConfig, OpenAPIArgs, DynamicRouterLoader as Router
-from .route_decorator import RouteDecorator, get, post, put, delete, page, get_auth, post_auth, put_auth, delete_auth, ws, ws_auth
+from .route_decorator import RouteDecorator, create_access_token, get, post, put, delete, page, get_auth, post_auth, put_auth, delete_auth, ws, ws_auth
 from .theme import ColorScheme, ThemeBuild
 from .reactive import use_state
