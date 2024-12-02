9d351c8c885ff18ddb55333a4e7545871b606cea
Pablo Schaffner <pablo@puntorigen.com>
2024-12-02 08:52:33 -0300

    - improved index example

--- example/routes/index.py
+++ example/routes/index.py
@@ -3,3 +3,11 @@ from nicegui_router import page, ui
 @page("/")
 def home():
     ui.markdown("Welcome to the Home Page!")
+    
+    def item(title, url):
+        # open page on click
+        return ui.item(title, on_click=lambda: ui.navigate.to(url))
+
+    with ui.list().props('dense separator'):
+        item("About", "/about")
+        item("Counter Example", "/counter")
